01366ea67ea8a0aae70c89bc067c60963a98b474
hwa <hwacode@163.com>
2017-03-22 23:42:29 +0800

    add

--- tesla/demo/day01.py
+++ tesla/demo/day01.py
@@ -61,7 +61,237 @@ print u'中国'
 print u'中国'.encode('utf-8')
 
 
+#关于编码的介绍
+#http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/001386819196283586a37629844456ca7e5a7faa9b94ee8000
+'''
+写u'中'和u'\u4e2d'是一样的，\u后面是十六进制的Unicode码。因此，u'A'和u'\u0041'也是一样的。
+两种字符串如何相互转换？字符串'xxx'虽然是ASCII编码，但也可以看成是UTF-8编码，而u'xxx'则只能是Unicode编码。
+把u'xxx'转换为UTF-8编码的'xxx'用encode('utf-8')方法：
+>>> u'ABC'.encode('utf-8')
+'ABC'
+>>> u'中文'.encode('utf-8')
+'\xe4\xb8\xad\xe6\x96\x87'
+英文字符转换后表示的UTF-8的值和Unicode值相等（但占用的存储空间不同），
+而中文字符转换后1个Unicode字符将变为3个UTF-8字符，你看到的\xe4就是其中一个字节，因为它的值是228，
+没有对应的字母可以显示，所以以十六进制显示字节的数值。
+反过来，把UTF-8编码表示的字符串'xxx'转换为Unicode字符串u'xxx'用decode('utf-8')方法：
 
+>>> 'abc'.decode('utf-8')
+u'abc'
+>>> '\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8')
+u'\u4e2d\u6587'
+>>> print '\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8')
+中文
+
+
+'''
+
+#格式化
+print 'hello %s' % 'world'
+print  'Hi, %s, you have $%d.' % ('Michael', 1000000)
+print  u'Hi, %s' % u'Michael'
+#   %运算符就是用来格式化字符串的。在字符串内部，%s表示用字符串替换，%d表示用整数替换,%f表示浮点数
+#  如果你不太确定应该用什么，%s永远起作用，它会把任何数据类型转换为字符串
+
+'''List'''
+'''list是一种有序的集合，可以随时添加和删除其中的元素。'''
+
+classmates = ['h','w','a','o','k']
+print classmates
+print len(classmates)  #用len()函数可以获得list元素的个数
+print classmates[0]
+print classmates[1]
+print classmates[len(classmates)-1]  #最后一个元素的索引是len(classmates) - 1
+print classmates[-1]  #可以用-1做索引，直接获取最后一个元素
+print classmates[-2]
+
+classmates.append('g')  #list中追加元素到末尾
+print classmates
+print classmates.insert(2,'gwh')  #把元素插入到指定的位置
+print classmates
+
+classmates.pop()  #删除list末尾的元素，用pop()方法
+print classmates
+
+classmates.pop(-1)  #删除指定位置的元素，用pop(i)方法，其中i是索引位置
+print classmates
+
+classmates[1] = 'love' #把某个元素替换成别的元素，可以直接赋值给对应的索引位置
+print classmates
+
+
+love = ['h',1234,True,23.03]  #list里面的元素的数据类型也可以不同
+print love
+
+aoe = ['python','java',['scala','javascript'],'c++']  #list元素也可以是另一个list
+print aoe
+
+aa = ['scala','javascript']
+aoe = ['python','java',aa,'c++']
+print aa[0]
+print aoe[2][0]   # 拿到scala的两种写法
+
+aaa = []  #一个空的list，它的长度为0
+print len(aaa)
+
+
+
+'''tuple'''
+'''tuple和list非常类似，但是tuple一旦初始化就不能修改'''
+classmates = ('h','l','g')
+print classmates
+
+#classmates这个tuple不能变了，它也没有append()，insert()这样的方法。
+# 其他获取元素的方法和list是一样的，你可以正常地使用classmates[0]，classmates[-1]，但不能赋值成另外的元素。
+
+# tuple 的陷阱
+t = (1,2)
+print t
+t = ()
+print t
+t = (1)
+print t
+# 当tuple中只有一个元素，需要在后面加一个逗号，否则会当成小括号来计算
+t = (1,)
+print t
+
+#可变的tuple
+t = ('a','b',['A','B'])
+print t
+t[2][0] = 'X'
+t[2][1] = 'Y'
+print t
+
+'''
+表面上看，tuple的元素确实变了，但其实变的不是tuple的元素，而是list的元素。
+tuple一开始指向的list并没有改成别的list，所以，tuple所谓的“不变”是说，
+tuple的每个元素，指向永远不变。即指向'a'，就不能改成指向'b'，指向一个list，
+就不能改成指向其他对象，但指向的这个list本身是可变的！
+'''
+
+
+# 条件判断
+# 注意if 和 else 后面都有冒号：
+age = 20
+print 'your age is:',age
+if age>=18:
+    print 'adult'
+elif age>=6:
+    print 'teenager'
+else:
+    print 'kid'
+
+'''
+if x:
+    print 'True'
+
+只要x是非零数值、非空字符串、非空list等，就判断为True，否则为False
+'''
+
+
+# 循环
+# for ...in 循环
+names = ['a','b','c','d']
+for name in names:
+    print name
+
+print range(5) # range()函数，可以生成一个整数序列，包左不包右
+
+sum = 0
+for x in range(101):
+    sum +=x
+print sum
+
+
+#dict 字典，即map
+dic = {'a':1,'b':2,'c':3}
+print dic['b']
+
+dic['d'] = 4 #根据key添加元素
+print dic
+
+# 如果key不存在，就会报错  print dic['e'] ，可以使用 in 来做判断
+print 'e' in dic   # False
+
+print dic.get('b')  # 也可以使用get方法
+print dic.get('f',200)  #如果key不存在，可以返回None，或者自己指定的value
+
+dic.pop('b')  #要删除一个key，用pop(key)方法
+print dic
+
+
+# set 只存储key，不存储value，并且元素不重复，重复元素在set中自动被过滤
+s= set(['a','b','c'])
+print s
+s.add(4)  # add()方法添加元素
+print s
+s.remove('b')  # remove()方法删除元素
+print s
+
+s1 = set([1,2,3])
+s2 = set([2,3,4])
+print s1 & s2    # set 无序无重复元素，所以可以做交集、并集等操作
+print s1 | s2
+
+
+slist = ['w','a','g'] # 可以把一个lsit放入一个set中
+s = set(slist)
+print s
+
+tu = ('w','a','g')  # 可以把一个tuple放入一个set中
+s = set(tu)
+print s
+
+
+#list 是可变对象
+a = ['c', 'b', 'a']
+a.sort()
+print a
+
+# str 是不可变对象
+a = 'abc'
+b = a.replace('a','A')
+print a
+print b
+
+
+
+#函数
+print cmp(1,3)  #自带函数
+print int('1234')  #类型转换函数
+print int(1234.1)
+
+def my_abs(x): # 利用def 自定义函数
+    if x>=0:
+        return x
+    else:
+        return -x
+
+print my_abs(-03)
+
+def nop():
+    pass   # 利用pass语句定义空函数，先占个坑，如果什么都不写会报错
+
+def my_abs(x):
+    if not isinstance(x,(int,float)):   # 利用isinstance做类型检查
+        raise TypeError('类型错误')
+    if x>=0:
+        return x
+    else:
+        return -x
+
+
+import math
+def move(x,y,step,angle=0):
+    nx = x+step*math.cos(angle)
+    ny = x+step*math.sin(angle)
+    return nx,ny          #返回多个值
+
+x,y = move(100,100,60,math.pi/6)
+print x,y
+
+r = move(100,100,60,math.pi/6)  #返回的多个值其实就是一个tuple，可以省略括号而已
+print r
 
 
 
--- tesla/demo/day02.py
+++ tesla/demo/day02.py
@@ -208,6 +208,403 @@ def add(x,y,f):
 print add(-5,5,abs)
 
 
+#map()
+# map将传入的函数依次作用到序列的每个元素，并把结果作为新的list返回
+# map()函数接收两个参数，一个是函数，一个是序列
+
+def f(x):
+    return x*x
+
+print map(f,[1,2,3,4,5])
+
+#reduce()
+#reduce把一个函数作用在一个序列[x1, x2, x3...]上，这个函数必须接收两个参数，
+# reduce把结果继续和序列的下一个元素做累积计算
+
+def add(x,y):
+    return x+y
+
+print reduce(add,[1,2,3,4,5])
+
+def fn(x,y):
+    return 10*x+y
+
+print reduce(fn,[1,2,3,4,5])   #12345
+
+
+#filter
+#filter()把传入的函数依次作用于每个元素，然后根据返回值是True还是False决定保留还是丢弃该元素。
+#filter()也接收一个函数和一个序列
+
+def is_odd(n):
+    return n%2==0
+
+print filter(is_odd,[1,2,3,4,5,6,7,8])  #[2, 4, 6, 8]
+
+#sorted()
+#可以接收一个比较函数来实现自定义的排序。
+
+print sorted([1,23,1,61,14,234,9])  #[1, 1, 9, 14, 23, 61, 234]
+
+def reverser_cmp(x,y):
+    if x>y:
+        return -1
+    if x<y:
+        return 1
+    return 0
+print sorted([1,23,1,61,14,234,9],reverser_cmp)  #[234, 61, 23, 14, 9, 1, 1]
+
+
+# 返回一个函数
+
+def lazy_sum(*args):
+    def sum():
+        s = 0
+        for n in args:
+            s=n+s
+        return s
+    return sum
+
+f = lazy_sum(1,2,3,4,5,6)  # 相当与返回了函数sum，把sum赋给了f，f=sum
+print f()   # 开始调用函数f
+
+f2 = lazy_sum(1,2,3,4,5,6)
+print f==f2  #False
+             # 当我们调用lazy_sum()时，每次调用都会返回一个新的函数，即使传入相同的参数
+
+#闭包
+#在上一个函数lazy_sum中，返回的函数sum其定义引用了局部变量args，
+# 所以，当一个函数返回了一个函数后，其内部的局部变量还被新函数引用
+
+#注意的问题，返回的函数并没有立刻执行，而是直到调用了f()才执行
+
+def count():
+    fs = []
+    for i in range(1,4):
+        def f():
+            return i*i
+        fs.append(f)
+    return fs
+
+f1,f2,f3 = count()
+print f1()  #9
+print f2()  #9
+print f3()  #9
+
+#可能认为结果是1,4,9 但却是9,9,9。原因就在于返回的函数引用了变量i，但它并非立刻执行。
+# 等到3个函数都返回时，它们所引用的变量i已经变成了3，因此最终结果为9。
+#分析：
+'''从网上看来的资料说，闭包 = 函数 + 引用环境，也就是说，当形成一个闭包之后，放进闭包的并不是具体的值。
+   以这一节的例子来看，闭包中应该只包含变量i的地址，告诉程序当他被调用时这个i应该从哪里找，此时并不涉及i的值。
+   只有当真正调用时，才根据此时i的值算出最终结果，而此时在返回3个函数之后，i的值已经成为3了，
+   所以当我们开始调用函数时，返回的值就都是9了'''
+
+#返回闭包时牢记的一点就是：返回函数不要引用任何循环变量，或者后续会发生变化的变量。
+#如果一定要引用循环变量怎么办？方法是再创建一个函数，
+# 用该函数的参数绑定循环变量当前的值，无论该循环变量后续如何更改，已绑定到函数参数的值不变
+
+def count():
+    fs=[]
+    for i in range(1,4):
+        def f(j):
+            def g():
+                return j*j
+            return g
+        fs.append(f(i))     #到这里，函数g用到的值，已经是函数f的参数了。而已绑定到函数参数的值不变
+    return fs
+f1,f2,f3 = count()
+print f1()   #1
+print f2()   #4
+print f3()   #9
+
+#匿名函数
+
+'''
+当我们在传入函数时，有些时候，不需要显式地定义函数，直接传入匿名函数更方便。
+在Python中，对匿名函数提供了有限支持。还是以map()函数为例，计算f(x)=x2时，除了定义一个f(x)的函数外，还可以直接传入匿名函数：
+>>> map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+[1, 4, 9, 16, 25, 36, 49, 64, 81]
+通过对比可以看出，匿名函数lambda x: x * x实际上就是：
+
+def f(x):
+    return x * x
+关键字lambda表示匿名函数，冒号前面的x表示函数参数。
+
+匿名函数有个限制，就是只能有一个表达式，不用写return，返回值就是该表达式的结果。
+用匿名函数有个好处，因为函数没有名字，不必担心函数名冲突。此外，匿名函数也是一个函数对象，也可以把匿名函数赋值给一个变量，再利用变量来调用该函数：
+
+>>> f = lambda x: x * x
+>>> f
+<function <lambda> at 0x10453d7d0>
+>>> f(5)
+25
+同样，也可以把匿名函数作为返回值返回，比如：
+def build(x, y):
+    return lambda: x * x + y * y
+'''
+
+#装饰器
+#decorator就是一个返回函数的高阶函数。
+def log(func):
+    def wrapper(*args,**kwargs):
+        print 'call %s():' % func.__name__
+        return func(*args,**kwargs)
+    return wrapper
+
+@log
+def now():
+    print '2017-03-22'
+
+now()
+
+'''
+调用now()函数，不仅会运行now()函数本身，还会在运行now()函数前打印一行日志：
+>>> now()
+call now():
+2013-12-25
+把@log放到now()函数的定义处，相当于执行了语句：
+
+now = log(now)
+由于log()是一个decorator，返回一个函数，所以，原来的now()函数仍然存在，只是现在同名的now变量指向了新的函数，
+于是调用now()将执行新函数，即在log()函数中返回的wrapper()函数。wrapper()函数的参数定义是(*args, **kw)，因此，
+wrapper()函数可以接受任意参数的调用。在wrapper()函数内，首先打印日志，再紧接着调用原始函数。
+
+如果decorator本身需要传入参数，那就需要编写一个返回decorator的高阶函数，写出来会更复杂。比如，要自定义log的文本：
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print '%s %s():' % (text, func.__name__)
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+这个3层嵌套的decorator用法如下：
+
+@log('execute')
+def now():
+    print '2013-12-25'
+执行结果如下：
+
+>>> now()
+execute now():
+2013-12-25
+
+我们来剖析上面的语句，首先执行log('execute')，返回的是decorator函数，再调用返回的函数，
+参数是now函数，返回值最终是wrapper函数。
+
+一个完整的decorator的写法如下：
+
+import functools
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print 'call %s():' % func.__name__
+        return func(*args, **kw)
+    return wrapper
+'''
+
+# 偏函数
+#把一个函数的某些参数给固定住（也就是设置默认值），返回一个新的函数，调用这个新函数会更简单。
+#例如：
+print int('1234')  #1234
+print int('1234',base=8)  #668  八进制
+print int('101010',base=2)  #42   二进制
+
+def int2(x):   # 定义一个int2函数，默认让他的base=2
+    return int(x,base=2)
+
+print int2('101010')
+
+import functools
+int8 = functools.partial(int,base=8)   #  functools.partial()可以方便的创建偏函数
+print int8('10')
+
+
+#模块
+'''请注意，每一个包目录下面都会有一个__init__.py的文件，这个文件是必须存在的，
+否则，Python就把这个目录当成普通目录，而不是一个包。__init__.py可以是空文件，
+也可以有Python代码，因为__init__.py本身就是一个模块，而它的模块名就是这个目录的名称。'''
+
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+' a test module '  #这前面是注释
+
+__author__ = 'Michael Liao'
+
+import sys
+
+def test():
+    args = sys.argv     #运行python hello.py获得的sys.argv就是['hello.py']；
+                        #运行python hello.py Michael获得的sys.argv就是['hello.py', 'Michael]。
+    if len(args)==1:
+        print 'Hello, world!'
+    elif len(args)==2:
+        print 'Hello, %s!' % args[1]
+    else:
+        print 'Too many arguments!'
+
+if __name__=='__main__':    #在命令行运行模块时生效，而在其他地方导入模块是，if判断将失效
+    test()
+
+
+
+#别名
+import cStringIO as StringIO   #import ... as ...指定了别名
+
+
+#作用域
+'''正常的函数和变量名是公开的（public），可以被直接引用，比如：abc，x123，PI等；
+类似__xxx__这样的变量是特殊变量，可以被直接引用，但是有特殊用途，比如上面的__author__，__name__就是特殊变量，
+hello模块定义的文档注释也可以用特殊变量__doc__访问，我们自己的变量一般不要用这种变量名；
+类似_xxx和__xxx这样的函数或变量就是非公开的（private），不应该被直接引用，比如_abc，__abc等；'''
+
+
+#类和实例
+
+class Student(object):
+
+    def __init__(self,name,score):   #__init__方法的第一个参数永远是self，表示创建的实例本身
+        self.name = name
+        self.score = score
+
+    def print_score(self):     #定义一个方法，除了第一个参数是self外，其他和普通函数一样。
+        print '%s:%s' %(self.name,self.score)
+
+bart = Student('Bart',8)
+list = Student('Lisa',9)
+bart.age = 20
+print bart.age  # 20
+#print list.age # 会报错，没有给lisa绑定age数据
+                # Python允许对实例变量绑定任何数据，也就是说，对于两个实例变量，虽然它们都是同一个类的不同实例，但拥有的变量名称都可能不同
+
+#访问限制
+
+class Student2(object):
+
+    def __init__(self,name,score):
+        self.__name = name     #实例的变量名如果以__开头，就变成了一个私有变量（private），只有内部可以访问，外部不能访问
+        self.__score = score
+
+    def get_name(self):      # 通过get、set方法来访问私有变量
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, score):
+        self.__score = score
+
+'''
+需要注意的是，在Python中，变量名类似__xxx__的，也就是以双下划线开头，并且以双下划线结尾的，是特殊变量，特殊变量是可以直接访问的，不是private变量，所以，不能用__name__、__score__这样的变量名。
+有些时候，你会看到以一个下划线开头的实例变量名，比如_name，这样的实例变量外部是可以访问的，但是，按照约定俗成的规定，当你看到这样的变量时，意思就是，“虽然我可以被访问，但是，请把我视为私有变量，不要随意访问”。
+双下划线开头的实例变量是不是一定不能从外部访问呢？其实也不是。不能直接访问__name是因为Python解释器对外把__name变量改成了_Student__name，所以，仍然可以通过_Student__name来访问__name变量：
+>>> bart._Student__name
+'Bart'
+但是强烈建议你不要这么干，因为不同版本的Python解释器可能会把__name改成不同的变量名。
+'''
+
+
+#继承和多态
+class Animal(object):  #在class的定义中，把父类放到括号中
+    def run(self):
+        print 'Animal is running...'
+
+class Dog(Animal):    #在class的定义中，把父类放到括号中
+    def run(self):    #重写了父类的run方法
+        print 'Dog is running...'
+    def eat(self):
+        print 'Eating meat...'
+
+class Cat(Animal):
+    def run(self):
+        print 'Cat is running...'
+
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+run_twice(Animal()) #多态
+run_twice(Dog())    #父类可以出现的地方，都可以放子类进去，并且调用的是子类的具体方法
+run_twice(Cat())
+
+
+#获取对象信息
+#使用type()
+'''
+>>> type(123)
+<type 'int'>
+>>> type('str')
+<type 'str'>
+>>> type(None)
+<type 'NoneType'>
+
+>>> type(abs)
+<type 'builtin_function_or_method'>
+>>> type(a)
+<class '__main__.Animal'>
+
+>>> import types
+>>> type('abc')==types.StringType
+True
+>>> type(u'abc')==types.UnicodeType
+True
+>>> type([])==types.ListType
+True
+>>> type(str)==types.TypeType
+True
+最后注意到有一种类型就叫TypeType，所有类型本身的类型就是TypeType，比如：
+
+>>> type(int)==type(str)==types.TypeType
+True
+
+'''
+
+#使用isinstance()
+#要判断class的类型，可以使用isinstance()函数。
+
+#使用dir()
+#要获得一个对象的所有属性和方法，可以使用dir()函数，它返回一个包含字符串的list，比如，获得一个str对象的所有属性和方法：
+#['__add__', '__class__', '__contains__', '__delattr__', '__doc__', '__eq__'......
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
